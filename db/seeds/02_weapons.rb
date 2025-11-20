456e5ac1f88d70edf61bf292ff354a2d13df2681
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-11-20 12:34:59 -0800

    feat(db): seed all current Weapons from v2.7 of the game

---
+++ db/seeds/02_weapons.rb
@@ -0,0 +1,558 @@
+# Order:
+# - Swords
+# - Broadblades
+# - Pistols
+# - Gauntlets
+# - Rectifiers
+#   - 5-stars
+#   - 4-stars
+#   - 3-stars
+#   - 2-stars
+#   - 1-stars
+
+# ===============================================
+# 02. WEAPONS
+# ===============================================
+puts "  --> Creating Weapons..."
+
+# --- Swords ---
+# 5-stars
+Weapon.find_or_initialize_by(name: "Blazing Brilliance").update!(
+  weapon_type: "Sword",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Bloodpact's Pledge").update!(
+  weapon_type: "Sword",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Defier's Thorn").update!(
+  weapon_type: "Sword",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Emerald of Genesis").update!(
+  weapon_type: "Sword",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Emerald Sentence").update!(
+  weapon_type: "Sword",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Red Spring").update!(
+  weapon_type: "Sword",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Unflicker Valor").update!(
+  weapon_type: "Sword",
+  rarity: 5
+)
+
+# 4-stars
+Weapon.find_or_initialize_by(name: "Commando of Conviction").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Endless Collapse").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Fables of Wisdom").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Feather Edge").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Lumingloss").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Lunar Cutter").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Overture").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Somnoire Anchor").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Sword#18").update!(
+  weapon_type: "Sword",
+  rarity: 4
+)
+
+# 3-stars
+Weapon.find_or_initialize_by(name: "Guardian Sword").update!(
+  weapon_type: "Sword",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Originite: Type II").update!(
+  weapon_type: "Sword",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Sword of Night").update!(
+  weapon_type: "Sword",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Sword of Voyager").update!(
+  weapon_type: "Sword",
+  rarity: 3
+)
+
+# 2-stars
+Weapon.find_or_initialize_by(name: "Tyro Sword").update!(
+  weapon_type: "Sword",
+  rarity: 2
+)
+
+# 1-stars
+Weapon.find_or_initialize_by(name: "Training Sword").update!(
+  weapon_type: "Sword",
+  rarity: 1
+)
+
+# --- Broadblades ---
+# 5-stars
+Weapon.find_or_initialize_by(name: "Ages of Harvest").update!(
+  weapon_type: "Broadblade",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Lustrous Razor").update!(
+  weapon_type: "Broadblade",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Thunderflare Dominion").update!(
+  weapon_type: "Broadblade",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Verdant Summit").update!(
+  weapon_type: "Broadblade",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Wildfire Mark").update!(
+  weapon_type: "Broadblade",
+  rarity: 5
+)
+
+# 4-stars
+Weapon.find_or_initialize_by(name: "Aureate Zenith").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Autumntrance").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Broadblade#41").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Dauntless Evernight").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Discord").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Helios Cleaver").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Meditations on Mercy").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Waning Redshift").update!(
+  weapon_type: "Broadblade",
+  rarity: 4
+)
+
+# 3-stars
+Weapon.find_or_initialize_by(name: "Beguiling Melody").update!(
+  weapon_type: "Broadblade",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Broadblade of Night").update!(
+  weapon_type: "Broadblade",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Broadblade of Voyager").update!(
+  weapon_type: "Broadblade",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Guardian Broadblade").update!(
+  weapon_type: "Broadblade",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Originite: Type I").update!(
+  weapon_type: "Broadblade",
+  rarity: 3
+)
+
+# 2-stars
+Weapon.find_or_initialize_by(name: "Tyro Broadblade").update!(
+  weapon_type: "Broadblade",
+  rarity: 2
+)
+
+# 1-stars
+Weapon.find_or_initialize_by(name: "Training Broadblade").update!(
+  weapon_type: "Broadblade",
+  rarity: 2
+)
+
+# --- Pistols ---
+# 5-stars
+Weapon.find_or_initialize_by(name: "Lux & Umbra").update!(
+  weapon_type: "Pistols",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Static Mist").update!(
+  weapon_type: "Pistols",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "The Last Dance").update!(
+  weapon_type: "Pistols",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Woodland Aria").update!(
+  weapon_type: "Pistols",
+  rarity: 5
+)
+
+# 4-stars
+Weapon.find_or_initialize_by(name: "Cadenza").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Novaburst").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Pistols#26").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Relativistic Jet").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Romance in Farewell").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Solar Flame").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Thunderbolt").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Undying Flame").update!(
+  weapon_type: "Pistols",
+  rarity: 4
+)
+
+# 3-stars
+Weapon.find_or_initialize_by(name: "Guardian Pistols").update!(
+  weapon_type: "Pistols",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Originite: Type III").update!(
+  weapon_type: "Pistols",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Pistols of Night").update!(
+  weapon_type: "Pistols",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Pistols of Voyager").update!(
+  weapon_type: "Pistols",
+  rarity: 3
+)
+
+# 2-stars
+Weapon.find_or_initialize_by(name: "Tyro Pistols").update!(
+  weapon_type: "Pistols",
+  rarity: 2
+)
+
+# 1-stars
+Weapon.find_or_initialize_by(name: "Training Pistols").update!(
+  weapon_type: "Pistols",
+  rarity: 1
+)
+
+# --- Gauntlets --- }
+# 5-stars
+Weapon.find_or_initialize_by(name: "Abyss Surges").update!(
+  weapon_type: "Gauntlets",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Blazing Justice").update!(
+  weapon_type: "Gauntlets",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Moongazer's Sigil").update!(
+  weapon_type: "Gauntlets",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Tragicomedy").update!(
+  weapon_type: "Gauntlets",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Verity's Handle").update!(
+  weapon_type: "Gauntlets",
+  rarity: 5
+)
+
+# 4-stars
+Weapon.find_or_initialize_by(name: "Aether Strike").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Amity Accord").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Celestial Spiral").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Gauntlets#21D").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Hollow Mirage").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Legend of Drunken Hero").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Marcato").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Stonard").update!(
+  weapon_type: "Gauntlets",
+  rarity: 4
+)
+
+# 3-stars
+Weapon.find_or_initialize_by(name: "Gauntlets of Night").update!(
+  weapon_type: "Gauntlets",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Gauntlets of Voyager").update!(
+  weapon_type: "Gauntlets",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Guardian Gauntlets").update!(
+  weapon_type: "Gauntlets",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Originite: Type IV").update!(
+  weapon_type: "Gauntlets",
+  rarity: 3
+)
+
+# 2-stars
+Weapon.find_or_initialize_by(name: "Tyro Gauntlets").update!(
+  weapon_type: "Gauntlets",
+  rarity: 2
+)
+
+# 1-stars
+Weapon.find_or_initialize_by(name: "Training Gauntlets").update!(
+  weapon_type: "Gauntlets",
+  rarity: 1
+)
+
+# --- Rectifiers ---
+# 5-stars
+Weapon.find_or_initialize_by(name: "Cosmic Ripples").update!(
+  weapon_type: "Rectifier",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Lethean Elegy").update!(
+  weapon_type: "Rectifier",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Luminous Hymn").update!(
+  weapon_type: "Rectifier",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Rime-Draped Sprouts").update!(
+  weapon_type: "Rectifier",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Stellar Symphony").update!(
+  weapon_type: "Rectifier",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Stringmaster").update!(
+  weapon_type: "Rectifier",
+  rarity: 5
+)
+
+Weapon.find_or_initialize_by(name: "Whispers of Sirens").update!(
+  weapon_type: "Rectifier",
+  rarity: 5
+)
+
+# 4-stars
+Weapon.find_or_initialize_by(name: "Augment").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Call of the Abyss").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Comet Flare").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Fusion Accretion").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Jinzhou Keeper").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Ocean's Gift").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Radiant Dawn").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Rectifier#25").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Variation").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+Weapon.find_or_initialize_by(name: "Waltz in Masquerade").update!(
+  weapon_type: "Rectifier",
+  rarity: 4
+)
+
+# 3-stars
+Weapon.find_or_initialize_by(name: "Guardian Rectifier").update!(
+  weapon_type: "Rectifier",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Originite: Type V").update!(
+  weapon_type: "Rectifier",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Rectifier of Night").update!(
+  weapon_type: "Rectifier",
+  rarity: 3
+)
+
+Weapon.find_or_initialize_by(name: "Rectifier of Voyager").update!(
+  weapon_type: "Rectifier",
+  rarity: 3
+)
+
+# 2-stars
+Weapon.find_or_initialize_by(name: "Tyro Rectifier").update!(
+  weapon_type: "Rectifier",
+  rarity: 2
+)
+
+# 1-stars
+Weapon.find_or_initialize_by(name: "Training Rectifier").update!(
+  weapon_type: "Rectifier",
+  rarity: 1
+)
+
+puts "  --> Weapons created succesfully."
