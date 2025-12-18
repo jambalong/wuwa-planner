45d8e030eeebd8dfe007a812e4384a210897d252
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-12-17 21:37:32 -0800

    feat(plans): generate planner_id if not exists and index plans based on planner_id

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,7 @@
 class PlansController < ApplicationController
   def index
+    @planner_id = cookies.permanent[:planner_id] ||= SecureRandom.uuid
+    @plans = Plan.where(planner_id: @planner_id).order(created_at: :desc)
   end
 
   def new
