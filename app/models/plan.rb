427f406676e4f0b63ab5742c47a686f8a8e3affe
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2025-12-31 13:12:35 -0800

    feat(plan): validation for plan must have owner through user_id or planner_id

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,13 @@
 class Plan < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  validate :must_have_owner
+
+  private
+
+  def must_have_owner
+    if user_id.blank? && planner_id.blank?
+      errors.add(:base, "Plan must belong to a user or a guest session")
+    end
+  end
 end
