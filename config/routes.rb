0bf54ac5b4f8748f27e66e0c599697fb9d9d0c51
John-Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2026-01-18 15:28:14 -0800

    refactor: styling, sync banner edit, and login route to sign-in route

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
 
   devise_for :users, skip: [ :sessions, :registrations ]
   devise_scope :user do
-    get "login", to: "devise/sessions#new", as: :new_user_session
-    post "login", to: "devise/sessions#create", as: :user_session
+    get "sign-in", to: "devise/sessions#new", as: :new_user_session
+    post "sign-in", to: "devise/sessions#create", as: :user_session
     delete "logout", to: "devise/sessions#destroy", as: :destroy_user_session
     get "signup", to: "devise/registrations#new", as: :new_user_registration
     post "signup", to: "devise/registrations#create", as: :user_registration
